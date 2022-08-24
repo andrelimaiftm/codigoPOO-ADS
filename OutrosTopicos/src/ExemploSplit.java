bfda6c8f5cbfdd3da21154f71122e901aea26e5e
andrelimaiftm <andrelima@iftm.edu.br>
2022-08-24 11:40:46 -0300

    exemplo do uso da função split

---
+++ OutrosTopicos/src/ExemploSplit.java
@@ -0,0 +1,18 @@
+public class ExemploSplit {
+
+    public static void main(String[] args) {
+        String s = "batata maca limao";
+
+        String[] vetor = s.split(" ");
+        String palavra1 = vetor[0];
+        String palavra2 = vetor[1];
+        String palavra3 = vetor[2];
+
+        System.out.println("mensagem original: " + s);
+        System.out.println("Após o uso do split");
+        System.out.println("palavra 1: " + vetor[0]);
+        System.out.println("palavra 2: " + vetor[1]);
+        System.out.println("palavra 3: " + vetor[2]);
+    }
+    
+}

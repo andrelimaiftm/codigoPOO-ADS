6abcc5f123abe107fc28767afae5bfe3bc6716c4
andrelimaiftm <andrelima@iftm.edu.br>
2022-08-25 13:27:47 -0300

    adição da classe Exemplo6 para aula de construtores

--- PooIntroducao/src/entidades/Produto.java
+++ PooIntroducao/src/entidades/Produto.java
@@ -4,28 +4,30 @@ public class Produto {
 
     public String nome;
     public double preco;
-    public int quantidade;
-
+    public int quantidade;    
+    
+    
     public double valorTotalEstoque(){
         return preco*quantidade;
     }
-
+    
     public void adicionaEstoque(int quantidade){
         this.quantidade += quantidade;
     }
-
+    
     public void removeEstoque(int quantidade){
         this.quantidade -= quantidade;
     }
-
+    
     public String toString(){
         return nome
-            + ", R$ "
-            + String.format("%.2f", preco)
-            +", "
-            +quantidade
-            +" unidades, Total: R$ "
-            + String.format("%.2f", valorTotalEstoque());
+        + ", R$ "
+        + String.format("%.2f", preco)
+        +", "
+        +quantidade
+        +" unidades, Total: R$ "
+        + String.format("%.2f", valorTotalEstoque());
+        
     }
     
 }
